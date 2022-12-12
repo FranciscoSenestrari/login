4ec450ee3b3e24c4bd24410afda2a0f346a1d2f3
FranciscoSenestrari <fran.senes@gmail.com>
2022-12-11 22:58:55 -0300

    animate login

--- src/index.js
+++ src/index.js
@@ -12,7 +12,50 @@ const firebaseConfig = {
   appId: "1:995175335328:web:958c6dc5ff6b4c3f42d0c5",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const wrapper = document.querySelector(".wrapper");
+const buttonRegiter = document.querySelector(".register");
+const buttonRenderLogin = document.querySelector(".btn-login");
 
+const renderRegister = () => {
+  const title = document.querySelector(".title");
+  const img = document.querySelector(".img-lore");
+  const btn = document.querySelector(".btn-login");
+  const regText = document.querySelector(".reg-text");
+  img.animate(
+    [
+      // fotogramas clave
+      { opacity: "100% " },
+      { opacity: "0%" },
+    ],
+    {
+      // opciones de sincronización
+      duration: 1000,
+      iterations: 1,
+    }
+  );
+  title.textContent = "Register";
+  wrapper.style.marginTop = "0.5%";
+  wrapper.style.height = "97%";
+  regText.textContent = "Already have an account?";
+  btn.value = "Register";
+  btn.addEventListener("click", renderLogin);
 
+  img.src = "/_dist_/pc-man.png";
+  console.log(img);
+};
+const renderLogin = () => {
+  /*
+  const wrapperCopy = wrapper;
+  const title = document.querySelector(".title");
+  const img = document.querySelector(".img-lore");
+  const btn = document.querySelector(".btn-login");
+  const regText = document.querySelector(".reg-text");
+  title.textContent = "Login";
+  wrapperCopy.style.marginTop = "4%";
+  wrapperCopy.style.height = " 80vh";
+  regText.textContent = "You have account?";
+  btn.value = "Login";
+  */
+};
+buttonRegiter.addEventListener("click", renderRegister);
