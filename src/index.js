f675213e4433f23cb36e6b86b8b190de7834a0b9
FranciscoSenestrari <fran.senes@gmail.com>
2022-12-13 01:26:01 -0300

    animate wrapper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { buttonRegister } from "./buttonRegister";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -14,48 +15,71 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const wrapper = document.querySelector(".wrapper");
-const buttonRegiter = document.querySelector(".register");
-const buttonRenderLogin = document.querySelector(".btn-login");
+const title = document.querySelector(".title");
+const img = document.querySelector(".img-lore");
+const btn = document.querySelector(".btn-LR");
+const regText = document.querySelector(".reg-text");
+const inputGroup = document.querySelector(".input-group");
+
+let users = [
+  {
+    userId: 1,
+    username: "frans",
+    email: "frans@frans.com",
+    password: "pass123",
+    name: "Francis",
+    lastname: "Senes",
+  },
+  {
+    userId: 2,
+    username: "luz42",
+    email: "lucy@lucy.com",
+    password: "pass123",
+    name: "Lucy",
+    lastname: "Senes",
+  },
+  {
+    userId: 3,
+    username: "juliet",
+    email: "juliet4@julies.com",
+    password: "pass123",
+    name: "Julliette",
+    lastname: "Salas",
+  },
+];
+localStorage.setItem("users", JSON.stringify(users));
 
 const renderRegister = () => {
-  const title = document.querySelector(".title");
-  const img = document.querySelector(".img-lore");
-  const btn = document.querySelector(".btn-login");
-  const regText = document.querySelector(".reg-text");
-  img.animate(
-    [
-      // fotogramas clave
-      { opacity: "100% " },
-      { opacity: "0%" },
-    ],
-    {
-      // opciones de sincronización
-      duration: 1000,
-      iterations: 1,
-    }
-  );
+  animate();
   title.textContent = "Register";
   wrapper.style.marginTop = "0.5%";
   wrapper.style.height = "97%";
   regText.textContent = "Already have an account?";
   btn.value = "Register";
   btn.addEventListener("click", renderLogin);
-
+  buttonRegister.textContent = "Login";
+  buttonRegister.removeEventListener("click", renderRegister);
+  buttonRegister.addEventListener("click", renderLogin);
   img.src = "/_dist_/pc-man.png";
-  console.log(img);
 };
 const renderLogin = () => {
-  /*
-  const wrapperCopy = wrapper;
-  const title = document.querySelector(".title");
-  const img = document.querySelector(".img-lore");
-  const btn = document.querySelector(".btn-login");
-  const regText = document.querySelector(".reg-text");
+  animate();
   title.textContent = "Login";
-  wrapperCopy.style.marginTop = "4%";
-  wrapperCopy.style.height = " 80vh";
+  wrapper.style.marginTop = "4%";
+  wrapper.style.height = " 80vh";
   regText.textContent = "You have account?";
   btn.value = "Login";
-  */
+  buttonRegister.textContent = "Register";
+  img.src = "/_dist_/meditate.png";
+  buttonRegister.removeEventListener("click", renderLogin);
+  buttonRegister.addEventListener("click", renderRegister);
 };
-buttonRegiter.addEventListener("click", renderRegister);
+const animate = () => {
+  wrapper.animate([{ opacity: "100% " }, { opacity: "20%" }], {
+    duration: 1000,
+    iterations: 1,
+  });
+};
+const login = () => {};
+const register = () => {};
+buttonRegister.addEventListener("click", renderRegister);
